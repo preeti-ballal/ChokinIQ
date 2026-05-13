f3e4b5779db7bf34d21b420caceb6d5b0a77c46c
Preeti Ballal <preetiballal.29@gmail.com>
2026-05-13 16:59:07 +0900

    Initial commit: Spring Boot + LINE + Gemini setup

---
+++ src/main/java/com/backend/chokiniq/ChokiniqApplication.java
@@ -0,0 +1,13 @@
+package com.backend.chokiniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChokiniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChokiniqApplication.class, args);
+	}
+
+}
